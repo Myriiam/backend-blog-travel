1ea9387103660efbd05e2fad604becab678c4256
Myriiam <kadiimyriam@hotmail.com>
2024-05-21 14:37:42 +0200

    Require image for articles

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -30,8 +30,8 @@ class ArticleController extends Controller
             'content' => 'required|string|max:255',
             'continent' => 'required|string',
             'country' => 'required|string',
-            'main_picture' => 'required|image|max:10240',
-            'images.*' => 'required|image|max:10240',
+            'main_picture' => 'required|max:10240',
+            'images.*' => 'required|max:10240',
             'categories' => 'required|exists:categories,id',
         ]);
 
