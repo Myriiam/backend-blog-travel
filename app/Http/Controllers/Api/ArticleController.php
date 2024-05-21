50a633b194fdef9deef687aefe3d9b8f845db23e
Myriiam <kadiimyriam@hotmail.com>
2024-05-21 13:54:02 +0200

    Add max size for images when posting an article

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -30,8 +30,8 @@ class ArticleController extends Controller
             'content' => 'required|string|max:255',
             'continent' => 'required|string',
             'country' => 'required|string',
-            'main_picture' => 'required|image',
-            'images' => 'required|image',
+            'main_picture' => 'required|image|max:10240',
+            'images.*' => 'required|image|max:10240',
             'categories' => 'required|exists:categories,id',
         ]);
 
@@ -178,9 +178,9 @@ class ArticleController extends Controller
         'content' => 'sometimes|string',
         'continent' => 'sometimes|string',
         'country' => 'sometimes|string',
-        'main_picture' => 'sometimes|image', // Max 10MB size
+        'main_picture' => 'sometimes|image|max:10240', // Max 10MB size
         'categories' => 'sometimes|exists:categories,id',
-        'images.*' => 'sometimes|image' // Max 10MB size per image
+        'images.*' => 'sometimes|image|max:10240' // Max 10MB size per image
     ]);
 
     if ($validator->fails()) {
